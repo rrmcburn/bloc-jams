4adcaf3ee8b463d12f05fa8ca87d9753d35ce7c4
Ryan McBurney <rrmcburn@gmail.com>
2017-02-04 12:09:19 -0500

    refactored collection and album page with jQuery

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,5 +1,7 @@
-var collectionItemTemplate =
-     '<div class="collection-album-container column fourth">'
+var buildCollectionItemTemplate = function() {
+     var template =
+    
+    '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
@@ -13,15 +15,23 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
+    return $(template);
+ };
 
-window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
+ $(window).load(function() {
+     
+    var $collectionContainer = $('.album-covers');
+
+     
+    $collectionContainer.empty();
+
  
-     // #3
+     
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+        
+        var $newThumbnail = buildCollectionItemTemplate();
+         $collectionContainer.append($newThumbnail);
+
+
      }
- }
+ });
